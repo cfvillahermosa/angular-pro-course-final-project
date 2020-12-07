009a066bb0af1584de871db833146013022639dd
Carlos Villa <carlos.fernandezv@vass.es>
2020-12-07 02:01:24 +0100

    AuthModule and child module setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
 import { Store } from 'store';
-
 // feature modules
-
+import { AuthModule } from './../auth/auth.module';
 // containers
 import { AppComponent } from './containers/app/app.component';
 
@@ -15,18 +13,22 @@ import { AppComponent } from './containers/app/app.component';
 export const ROUTES: Routes = [];
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(ROUTES)
-  ],
-  declarations: [
-    AppComponent
-  ],
-  providers: [
-    Store
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES), AuthModule],
+  declarations: [AppComponent],
+  providers: [Store],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+/*
+ var firebaseConfig = {
+    apiKey: "AIzaSyAoGAisqpUsB713X_0_tBHBTuTBQtUIia0",
+    authDomain: "fitness-app-6280c.firebaseapp.com",
+    databaseURL: "https://fitness-app-6280c.firebaseio.com",
+    projectId: "fitness-app-6280c",
+    storageBucket: "fitness-app-6280c.appspot.com",
+    messagingSenderId: "310547498801",
+    appId: "1:310547498801:web:ac179239d24cc8fe2b5210",
+    measurementId: "G-TK43VS502R"
+  };
+*/
---
+++ src/auth/register/register.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './containers/register/register.component';
+export const ROUTES: Routes = [{ path: '', component: RegisterComponent }];
+
+@NgModule({
+  imports: [CommonModule, RouterModule],
+  declarations: []
+})
+export class RegisterModule {}
