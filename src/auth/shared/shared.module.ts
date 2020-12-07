a732e9c2086d839722de2867862045cb010a6916
Carlos Villa <carlos.fernandezv@vass.es>
2020-12-07 20:44:23 +0100

    Implementing AuthGuards for lazy routes

--- src/auth/shared/shared.module.ts
+++ src/auth/shared/shared.module.ts
@@ -5,6 +5,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AuthFormComponent } from './components/auth-form/auth-form.component';
 // services
 import { AuthService } from './services/auth/auth.service';
+//guards
+import { AuthGuard } from './guards/auth.guard';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule],
@@ -16,7 +18,7 @@ export class SharedModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: SharedModule,
-      providers: [AuthService]
+      providers: [AuthService, AuthGuard]
     };
   }
 }
