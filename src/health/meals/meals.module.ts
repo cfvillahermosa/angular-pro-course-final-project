efd0d0f703d4b7f8c9c803bc5d2e0ce58e60305d
Carlos Villa <carlos.fernandezv@vass.es>
2020-12-08 01:04:53 +0100

    Component architecture and Reactive Forms

--- src/health/meals/meals.module.ts
+++ src/health/meals/meals.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
+//components
+import { MealFormComponent } from './components/meal-form/meal-form.component';
+
+
 // containers
 import { MealsComponent } from './containers/meals/meals.component';
+import { MealComponent } from './containers/meal/meal.component';
 
-export const ROUTES: Routes = [{ path: '', component: MealsComponent }];
-
+export const ROUTES: Routes = [
+  { path: '', component: MealsComponent },
+  { path: 'new', component: MealComponent }
+];
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(ROUTES), SharedModule],
-  declarations: [MealsComponent]
+  declarations: [MealsComponent, MealComponent, MealFormComponent]
 })
 export class MealsModule {}
