b474a97d5853829711f94a9306ff758353de5a08
Carlos Villa <carlos.fernandezv@vass.es>
2020-12-08 12:52:17 +0100

    Route Params and Observable switchMaps

--- src/health/meals/containers/meal/meal.component.ts
+++ src/health/meals/containers/meal/meal.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { Meal, MealsService } from './../../../shared/services/meals/meals.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/switchMap';
+
+import { MealsService, Meal } from '../../../shared/services/meals/meals.service';
 
 @Component({
   selector: 'meal',
@@ -10,17 +15,32 @@ import { Meal, MealsService } from './../../../shared/services/meals/meals.servi
       <div class="meal__title">
         <h1>
           <img src="/img/food.svg" />
-          <span>Create meal</span>
+          <span *ngIf="meal$ | async as meal; else title"> {{ meal.name ? 'Edit' : 'Create' }} meal </span>
+          <ng-template #title>
+            Loading...
+          </ng-template>
         </h1>
       </div>
       <div>
-        <meal-form (create)="addMeal($event)"></meal-form>
+        <meal-form (create)="addMeal($event)"> </meal-form>
       </div>
     </div>
   `
 })
-export class MealComponent {
-  constructor(private mealsService: MealsService, private router: Router) {}
+export class MealComponent implements OnInit, OnDestroy {
+  meal$: Observable<Meal>;
+  subscription: Subscription;
+
+  constructor(private mealsService: MealsService, private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.subscription = this.mealsService.meals$.subscribe();
+    this.meal$ = this.route.params.switchMap(param => this.mealsService.getMeal(param.id) as Observable<Meal>);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
   async addMeal(event: Meal) {
     await this.mealsService.addMeal(event);
--- src/health/shared/services/meals/meals.service.ts
+++ src/health/shared/services/meals/meals.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
-import 'rxjs/add/operator/do';
-import { Observable } from 'rxjs/Observable';
+
 import { Store } from 'store';
-import { AuthService } from './../../../../auth/shared/services/auth/auth.service';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
+
+import { AuthService } from '../../../../auth/shared/services/auth/auth.service';
 
 export interface Meal {
   name: string;
@@ -15,14 +21,20 @@ export interface Meal {
 
 @Injectable()
 export class MealsService {
-  meals$: Observable<Meal[]> = this.db
-    .list(`meals/${this.uid}`)
-    .do(next => this.store.set('meals', next)) as Observable<Meal[]>;
+  meals$: Observable<Meal[]> = this.db.list(`meals/${this.uid}`).do(next => this.store.set('meals', next));
 
   constructor(private store: Store, private db: AngularFireDatabase, private authService: AuthService) {}
 
   get uid() {
     return this.authService.user.uid;
+  }
+
+  getMeal(key: string) {
+    if (!key) return Observable.of({});
+    return this.store
+      .select<Meal[]>('meals')
+      .filter(Boolean)
+      .map((meals: Meal[]) => meals.find((meal: Meal) => meal.$key === key));
   }
 
   addMeal(meal: Meal) {
